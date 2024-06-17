3d777345d9d97d0eafdd737076b5f9469305390e
Jezidmar <102309599+Jezidmar@users.noreply.github.com>
2024-06-17 22:50:52 +0200

    Update unfold.m

--- Preprocess/unfold.m
+++ Preprocess/unfold.m
@@ -10,7 +10,4 @@ M2 = reshape(permute(K, [2 1 3]), [dim2 dim1*dim3]);
 % Unfold T in the third mode (i.e., keeping the third dimension fixed)
 M3 = reshape(permute(K, [3 1 2]), [dim3 dim1*dim2]);
 
-
-%Ovo radi.
-
 %B = nmodeproduct(K, U, n)
